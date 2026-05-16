9fabb3e025e776a4a328ac22ac19abcf6ec80e5f
Hidetake Iwata <int128@gmail.com>
2026-05-16 16:16:17 +0900

    Generate relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Generate the relative paths since the app is hosted at GitHub Pages.
+  base: '',
 })
