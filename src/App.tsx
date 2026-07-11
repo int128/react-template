61299765565d04c66c12f4026f0654716625b048
Hidetake Iwata <int128@gmail.com>
2026-07-11 11:50:46 +0900

    Fix lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
             <li>
               <a href="https://react.dev/" target="_blank" rel="noopener">
                 <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
+                Learn React
               </a>
             </li>
           </ul>
